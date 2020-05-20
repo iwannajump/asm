d550cda429b9ddb8eeaa523d44390826606ac420
iwannajump <59066701+iwannajump@users.noreply.github.com>
2020-05-20 11:48:54 +0200

    Update print_message_macros.asm

--- print_message_macros.asm
+++ print_message_macros.asm
@@ -2,11 +2,9 @@ format ELF64
 public _start
 
 section '_data' writable
-	asm_message_1 db "assembly_test_string", 	0xA, 0
-								; 0xA - print new line
+	asm_message_1 db "assembly_test_string", 	0xA, 0	; 0xA - print new line
 
-	asm_message_2 db "assembly_test_string 1", 	0xA, 0
-								; 0 - \0
+	asm_message_2 db "assembly_test_string 1", 	0xA, 0	; 0 - \0
 
 section '_text' executable
 	_start:
@@ -24,11 +22,11 @@ section '_print_string' executable
 		push rbx					; push value to the stack
 		push rcx					; push value to the stack
 		push rdx					; push value to the stack
-		mov  rcx, rax				; move pointer to first symbol of message to RAX
+		mov  rcx, rax					; move pointer to first symbol of message to RAX
 		call _length_string
-		mov rdx, rax 				; RAX contains result of _length_string
+		mov rdx, rax 					; RAX contains result of _length_string
 		mov rax, 4					; 4 - write
-		xor rbx, rbx				; 1 - stdout
+		xor rbx, rbx					; 1 - stdout
 		inc rbx
 		int 0x80					; call system kernel
 		pop rdx						; get value from the stack
@@ -40,14 +38,14 @@ section '_print_string' executable
 section '_length_string' executable
 	_length_string:
 		push rdx
-		xor rdx, rdx				; mov RDX, 0 
-		._next_iteration:			; internal procedure
-			cmp [rax+rdx], byte 0	; RAX - message, RDX - message length
+		xor rdx, rdx					; mov RDX, 0 
+		._next_iteration:				; internal procedure
+			cmp [rax+rdx], byte 0			; RAX - message, RDX - message length
 			je .close				; if cmp = true then 
 			inc rdx					; get next char
-			jmp ._next_iteration	; goto _next_iteration
+			jmp ._next_iteration			; goto _next_iteration
 		.close:
-			mov rax, rdx			; push result of _next_iteration to RAX
+			mov rax, rdx				; push result of _next_iteration to RAX
 			pop rdx					; get value from the stack
 			ret 					; end of macros
 
