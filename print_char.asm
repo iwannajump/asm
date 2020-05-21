09dd52eee652fe117508dff5c3fc504339fee947
iwannajump <59066701+iwannajump@users.noreply.github.com>
2020-05-21 11:23:25 +0200

    Update print_char.asm

--- print_char.asm
+++ print_char.asm
@@ -4,7 +4,7 @@ public _start
 	bs_char rb 1
 
 _start:
-	mov rax, '@'
+	mov   rax, '@'
 	call _print_char
 	call _new_line
 
@@ -15,16 +15,16 @@ _print_char:
 	push rbx
 	push rcx
 	push rdx
-	mov [bs_char], al
-	mov rax, 4
-	mov rbx, 1
-	mov rcx, bs_char
-	mov rdx, 1
-	int 0x80
-	pop rdx
-	pop rcx
-	pop rbx
-	pop rax
+	mov  [bs_char], al
+	mov  rax, 4
+	mov  rbx, 1
+	mov  rcx, bs_char
+	mov  rdx, 1
+	int  0x80
+	pop  rdx
+	pop  rcx
+	pop  rbx
+	pop  rax
 	ret
 	
 _exit:
